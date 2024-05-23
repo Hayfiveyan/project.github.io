cf1e3d7efc46e2026d0c30f08124c37f50ddef6f
Hayfiveyan <165126716+Hayfiveyan@users.noreply.github.com>
2024-05-23 14:14:02 +0700

    Add files via upload

---
+++ c++-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main(){
+	
+  /*string str1,str2,str3;
+str3="I with you";
+str1="I Love You momy!";
+str2="I want to married with u!";
+cout<<"you answer to your :\n"<<str1;
+cout<<"\nyour answer to your :\n"<<str2;
+cout<<"\nyour answer to your :\n"<<str3;*/
+/*int a;
+cout<<"Enter the value:";
+cin>>a;
+a =pow(a,2);
+a=a*a;
+cout<<"the power is:"<<a;*/
+
+float pi =3.14159;
+float r=2.5;
+float area;
+area =pi*pow(r,2);
+
+cout<<"this is Area :"<<area;
+
+return 0;
+}
